1e5ffff1e3eb1509a5d36783c2417427d886d236
Deyine <deyine.jiddou@gmail.com>
2015-08-30 17:54:46 +0000

    change migration table name

--- config/cleaner-config.js
+++ config/cleaner-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mysql: {
-    skipTables: ['schema_migrations']
+    skipTables: ['migrations']
   },
 
   postgresql: {
-    skipTables: ['schema_migrations']
+    skipTables: ['migrations']
   }
 }
